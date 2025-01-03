a5c0041b4bb4cb20a0a82322283aeafca3b6c65f
Allen Luis Alvarez <allen.luis626@gmail.com>
2025-01-03 22:11:09 +0800

    test

--- frontend/Admin/admin.js
+++ frontend/Admin/admin.js
@@ -1043,6 +1043,7 @@ document.addEventListener("DOMContentLoaded", async function () {
                             numberOfweek = parseInt(item.weekly)
                             weeks = Math.ceil(((numberOfweek - 1) / 4 + 1) / 4 + 1);
                             console.log(weeks);
+                            console.log(numberOfweek);
     
                             if(weeks == 1){
                                 chartData = {
