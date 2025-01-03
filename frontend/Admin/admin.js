2be0f961997b069810f372e8e0a8c94bafa29a49
Allen Luis Alvarez <allen.luis626@gmail.com>
2025-01-03 23:53:10 +0800

    test

--- frontend/Admin/admin.js
+++ frontend/Admin/admin.js
@@ -1115,7 +1115,7 @@ document.addEventListener("DOMContentLoaded", async function () {
                     if (monthlyy.length > 1 ) {
                         // check the first number of months to start then prompt it then fill the 0 of null data.
                         monthlyy.forEach((i, index) => {
-                            if (i.monthly[0] != NULL) {
+                            if (i.monthly[0] !== NULL) {
                                 const monthsCount  = new Array(12).fill(0);
                                 if (index === i.monthly[0]) {
                                     monthsCount[index] = i.monthly[index];
